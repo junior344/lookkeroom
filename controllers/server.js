c77c52426c825c0bace90b688a571c3b995cf15c
josias <josiasmbogle237@gmail.com>
2024-10-18 20:25:53 +0200

    second commit

--- controllers/server.js
+++ controllers/server.js
@@ -13,9 +13,11 @@ const pool = mariadb.createPool({
     database: 'lokker',
     port: 3307,
 });
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+// Test de connexion à la base de données
 (async () => {
     try {
         const connection = await pool.getConnection();
@@ -39,6 +41,85 @@ const createUser = async (email, hashedPassword) => {
         throw error;
     }
 };
+
+const createLobby = async (req, res) => {
+    const { name } = req.body;
+    try {
+      const result = await pool.query(
+        'INSERT INTO lobbies (name, admin_id) VALUES ($1, $2) RETURNING *',
+        [name, req.user.userId]
+      );
+      res.status(201).json({ lobby: result.rows[0] });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+};
+const authenticate = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if(!token) return res.status(401).json({ message: 'No token provided' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err) return res.status(401).json({ message: 'Unauthorized' });
+        req.user = user;
+        next();
+    });
+}
+app.post('/api/lobby', authenticate, async (req, res) => {
+    const {name} = req.body;
+    const admin_id = req.user.user.id;
+
+    try {
+        const result = await pool.query(
+            'INSERT INTO lobbies (name, admin_id) VALUES (?, ?)',
+            [name, admin_id]
+        );
+        res.status(201).json({message: 'lobby cree', lobby: result });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+// Récupérer tous les lobbies d'un utilisateur
+app.get('api/lobby/:id',authenticate, async (req, res) => {
+    const { id } = req.params.id;
+    try {
+        const result = await pool.query(
+            'SELECT * FROM lobbies WHERE id = ?',
+            [id]
+        );
+        res.json({ lobby: result });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+//poster un message dans un lobby
+app.post('/api/lobby/:id/:message', authenticate, async (req, res) => {
+    const { id } = req.params.id;
+    const { message } = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO messages (lobby_id, user_id, message) VALUES (?, ?, ?)',
+            [id, req.user.userId, message]
+        );
+        res.status(201).json({ message: 'message cree', message: result });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+//Modifier un message
+app.patch('/api/lobby/:id/:message', authenticate, async (req, res) => {
+    const { id } = req.params.id;
+    const { message } = req.body;
+    try {
+        const result = await pool.query(
+            'UPDATE messages SET message = ? WHERE id = ?',
+            [message, id]
+        );
+        res.json({ message: 'message modifie', message: result });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
 app.get('/', (req, res) =>{
     res.send('welcome to my chat app');
 })
@@ -55,11 +136,11 @@ app.post('/api/register', async (req, res) => {
         res.status(500).json({ message: 'Error creating user', error });
     }
 });
+
 const getUserByEmail = async (email) => {
     try {
         const result = await pool.query(
             `SELECT * FROM user WHERE email = "${email}"`,
-            
         );
         if (result.length > 0) {
             return result[0]; // Renvoyez le premier utilisateur trouvé
@@ -71,6 +152,7 @@ const getUserByEmail = async (email) => {
         throw error;
     }
 };
+
 app.post('/api/login', async (req, res) => {
     console.log('Request body:', req.body);
     const { email, password } = req.body;
@@ -82,7 +164,7 @@ app.post('/api/login', async (req, res) => {
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
 });
-
+app.get('/api/lobby/:lobby-id', createLobby);
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 })
